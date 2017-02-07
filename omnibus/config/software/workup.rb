aa773e2fa74c944041dbc1834e70d0d8fbf8ff9e
Montague, Brent <bmontague@cvent.com>
2017-02-06 19:00:41 -0500

    Add more dependencies and cleanup builds

--- omnibus/config/software/workup.rb
+++ omnibus/config/software/workup.rb
@@ -15,41 +15,31 @@
 # limitations under the License.
 #
 
-# These options are required for all software definitions
+require_relative '../../../lib/workup/version.rb'
+
 name 'workup'
-default_version 'local_source'
+default_version Workup::VERSION
 
-# For the specific super-special version "local_source", build the source from
-# the local git checkout. This is what you'd want to occur by default if you
-# just ran omnibus build locally.
-version('local_source') do
-  source path: File.expand_path('../..', project.files_path),
-         # Since we are using the local repo, we try to not copy any files
-         # that are generated in the process of bundle installing omnibus.
-         # If the install steps are well-behaved, this should not matter
-         # since we only perform bundle and gem installs from the
-         # omnibus cache source directory, but we do this regardless
-         # to maintain consistency between what a local build sees and
-         # what a github based build will see.
-         options: { exclude: ['omnibus/vendor'] }
-end
-
-# For any version other than "local_source", fetch from github.
-source git: 'git://github.com/cvent/workup.git' if version != 'local_source'
+source path: File.expand_path('../..', project.files_path),
+       options: { exclude: ['omnibus/vendor'] }
 
 # For nokogiri
-#dependency 'libxml2'
+dependency 'libxml2'
 dependency 'libxslt'
-#dependency 'libiconv'
-#dependency 'liblzma'
-#dependency 'zlib'
+dependency 'libiconv'
+dependency 'liblzma'
+dependency 'zlib'
 
 # ruby and bundler and friends
 dependency 'ruby'
-dependency 'ruby-windows-devkit' if windows?
 dependency 'rubygems'
 dependency 'bundler'
-dependency 'git-windows' if windows?
+dependency 'appbundler'
+
+if windows?
+  dependency 'ruby-windows-devkit'
+  dependency 'git-windows'
+end
 
 # Version manifest file
 dependency 'version-manifest'
@@ -57,8 +47,16 @@ dependency 'version-manifest'
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
-  gem 'build workup.gemspec', env: env
-  gem 'install workup*.gem', env: env
+  delete "#{name}-*.gem"
+
+  # We bundle install to ensure the versions of gems we are going to
+  # appbundle-lock to are definitely installed
+  bundle 'install', env: env
+
+  gem "build #{name}.gemspec", env: env
+  gem "install #{name}-*.gem --no-document", env: env
+
+  appbundle 'workup', env: env
 
   block do
     ['workup', 'git'].each do |cmd|
